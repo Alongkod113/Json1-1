3e39b6389593e106e9a4b7cbaa163c840e4ce499
Teerapoom <88318468+Teerapoom@users.noreply.github.com>
2021-12-07 11:05:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,16 +13,28 @@
     Sname: <span id="lname"></span>
 </body>
 <script>
+// function loadJSON(){
+//     var url = "https://cj-android-demon.herokuapp.com/json1.php";
+//     $.get(url, (data, status)=>{
+//         console.log(data);
+//         var jData = JSON.parse(data);
+//         console.log(jData.fname);
+//         console.log(jData.lname);
+//         $("#fname").text(jData.fname);
+//         $("#lname").text(jData.lname);
+//     });
+// }
+// $(()=>{
+//     $("#btnJSON1").click(loadJSON);
+// });
 function loadJSON(){
     var url = "https://cj-android-demon.herokuapp.com/json1.php";
-    $.get(url, (data, status)=>{
-        console.log(data);
-        var jData = JSON.parse(data);
-        console.log(jData.fname);
-        console.log(jData.lname);
-        $("#fname").text(jData.fname);
-        $("#lname").text(jData.lname);
-    });
+    $.getJSON(url)
+        .done((data)=>{
+            console.log(data);
+        })
+        .fail((xhr, status, err)=>{
+        });
 }
 $(()=>{
     $("#btnJSON1").click(loadJSON);
